ae20df9e173ae53f99b6823bfa50331df609a52b
MikiZenebe <mickyaszenebe@gmail.com>
2024-03-11 00:12:44 -0500

    add & remove cart

--- store.ts
+++ store.ts
@@ -7,6 +7,7 @@ type CartState = {
   cart: AddCartType[];
   toggleCart: () => void;
   addProduct: (item: AddCartType) => void;
+  removeProduct: (item: AddCartType) => void;
 };
 
 export const useCartStore = create<CartState>()(
@@ -15,6 +16,7 @@ export const useCartStore = create<CartState>()(
       cart: [],
       isOpen: false,
       toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
+
       addProduct: (item) =>
         set((state) => {
           const existingItem = state.cart.find(
@@ -31,6 +33,29 @@ export const useCartStore = create<CartState>()(
             return { cart: updatedCart };
           } else {
             return { cart: [...state.cart, { ...item, quantity: 1 }] };
+          }
+        }),
+
+      removeProduct: (item) =>
+        set((state) => {
+          //Check if the item exist and remove quantitiy -1
+          const existingItem = state.cart.find(
+            (cartItem) => cartItem.id === item.id
+          );
+          if (existingItem && existingItem.quantity! > 1) {
+            const updatedCart = state.cart.map((cartItem) => {
+              if (cartItem.id === item.id) {
+                return { ...cartItem, quantity: cartItem.quantity! - 1 };
+              }
+              return cartItem;
+            });
+            return { cart: updatedCart };
+          } else {
+            //Remove Item from cart
+            const filterdCart = state.cart.filter(
+              (cartItem) => cartItem.id !== item.id
+            );
+            return { cart: filterdCart };
           }
         }),
     }),
